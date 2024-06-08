ba61ba8fbeccaf60d0a4a38ebc3c260c74fc8248
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-06-08 01:05:35 -0300

    feat: add subtitle

---
+++ src/components/subtitle/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './style';
+
+export default function Subtitle({ children }: { children: React.ReactNode }) {
+    return <S.Subtitle>{children}</S.Subtitle>;
+}
---
+++ src/components/subtitle/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import theme from '../../styles/theme';
+
+export const Subtitle = styled.h3`
+    font-size: 36px;
+    font-weight: ${theme.fonts.weight.semiBold};
+    line-height: 48px;
+    letter-spacing: 1.44px;
+    color: ${theme.dark};
+`;
