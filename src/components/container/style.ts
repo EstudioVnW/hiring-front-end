47d632b9409570cde058e04de4574a3b081a5107
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-06-08 12:30:33 -0300

    feat: add container component

---
+++ src/components/container/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+    max-width: 1366px;
+    margin: 0 auto;
+    padding: 40px 32px;
+`;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,10 @@
+import Container from '../../components/container';
+import OurProducts from './components/ourProducts';
+
 export default function Home() {
-    return <main>Home</main>;
+    return (
+        <Container>
+            <OurProducts />
+        </Container>
+    );
 }
