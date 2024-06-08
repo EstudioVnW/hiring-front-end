b92ef0c28e1f1af74c25a2b69e638a1b241551ff
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-06-07 23:08:09 -0300

    feat: add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,61 @@
+import { List, MagnifyingGlass, ShoppingCart, X } from '@phosphor-icons/react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { navLinks } from '../../mocks/navLinks';
+import * as S from './style';
+
 export default function Header() {
-    return <div>Header</div>;
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <S.Header>
+            <S.WinterPromoSection>
+                <S.PromoText>
+                    Promoção de inverno para todos os trajes de frio e entrega
+                    expressa grátis - OFF 50%!
+                </S.PromoText>
+            </S.WinterPromoSection>
+
+            <S.HeaderSection>
+                <S.BoxLinks>
+                    <Link to="/">
+                        <S.Title>Exlusive</S.Title>
+                    </Link>
+
+                    <S.HeaderNav menuOpen={menuOpen}>
+                        {navLinks.map((item) => (
+                            <Link to={item.link} key={item.name}>
+                                {item.name}
+                            </Link>
+                        ))}
+                    </S.HeaderNav>
+
+                    <S.MenuButton onClick={toggleMenu}>
+                        {menuOpen ? <X size={32} /> : <List size={32} />}
+                    </S.MenuButton>
+
+                    <S.HeaderBoxSearch>
+                        <S.SearchBar>
+                            <S.InputSearch
+                                type="text"
+                                placeholder="O que você está procurando?"
+                            />
+                            <S.BtnTransparent search type="submit">
+                                <MagnifyingGlass size={20} />
+                            </S.BtnTransparent>
+                        </S.SearchBar>
+
+                        <S.BtnTransparent>
+                            <ShoppingCart size={24} />
+                        </S.BtnTransparent>
+                    </S.HeaderBoxSearch>
+                </S.BoxLinks>
+            </S.HeaderSection>
+        </S.Header>
+    );
 }
---
+++ src/components/header/style.ts
@@ -0,0 +1,159 @@
+import styled, { css } from 'styled-components';
+
+import devices from '../../styles/devices';
+import theme from '../../styles/theme';
+
+export const Header = styled.header`
+    border-bottom: 1px solid ${theme.dark};
+`;
+
+export const WinterPromoSection = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    padding: 0 32px;
+    background: ${theme.dark};
+
+    @media (${devices.tablet}) {
+        display: none;
+    }
+`;
+
+export const PromoText = styled.p`
+    text-align: center;
+    font-size: 14px;
+    line-height: 21px;
+    color: ${theme.lightForegroud};
+
+    @media (${devices.tablet}) {
+        font-size: 12px;
+    }
+`;
+
+export const HeaderSection = styled.section`
+    position: relative;
+    max-width: 1366px;
+    margin: 0 auto;
+    padding: 0 32px;
+`;
+
+export const BoxLinks = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 8px;
+    min-height: 60px;
+    padding: 12px 0;
+`;
+
+export const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 1.5;
+    letter-spacing: 0.72px;
+    color: ${theme.dark};
+`;
+
+export const HeaderNav = styled.nav<{ menuOpen: boolean }>`
+    display: flex;
+    gap: 48px;
+    transition: all 0.3s ease-in-out;
+
+    a {
+        font-size: 16px;
+        line-height: 24px;
+        color: ${theme.dark};
+        transition: color 0.3s ease-in-out;
+
+        &:hover {
+            opacity: 0.8;
+        }
+    }
+
+    @media (${devices.laptop}) {
+        gap: 12px;
+    }
+
+    @media (${devices.tablet}) {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 100;
+        flex-direction: column;
+        background: ${theme.lightForegroud};
+        padding: 100px 20px 20px 20px;
+        transform: ${({ menuOpen }) =>
+            menuOpen ? 'translateY(0)' : 'translateY(-100%)'};
+        opacity: ${({ menuOpen }) => (menuOpen ? '1' : '0')};
+        visibility: ${({ menuOpen }) => (menuOpen ? 'visible' : 'hidden')};
+
+        a {
+            font-size: 18px;
+        }
+    }
+`;
+
+export const MenuButton = styled.button`
+    z-index: 999;
+    display: none;
+    background: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
+
+    @media (${devices.tablet}) {
+        display: block;
+    }
+`;
+
+export const HeaderBoxSearch = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 24px;
+
+    @media (${devices.laptop}) {
+        gap: 12px;
+    }
+
+    @media (${devices.tablet}) {
+        width: 100%;
+    }
+`;
+
+export const SearchBar = styled.form`
+    position: relative;
+    width: 250px;
+
+    @media (${devices.tablet}) {
+        width: 100%;
+    }
+`;
+
+export const InputSearch = styled.input`
+    width: 100%;
+    height: 38px;
+    padding: 12px 32px 12px 12px;
+    border: none;
+    outline: none;
+    border-radius: 4px;
+    background: ${theme.lightForegroud};
+`;
+
+export const BtnTransparent = styled.button<{ search?: boolean }>`
+    background: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
+
+    ${({ search }) =>
+        search &&
+        css`
+            position: absolute;
+            right: 8px;
+            height: 40px;
+        `}
+`;
