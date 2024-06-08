bc02939704fb8d681a40a513ebb1663f86103750
mateusisrael <mateuss.i.fernandes@gmail.com>
2024-06-08 13:51:50 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
+
+export default App
