bc3afa3991140c65a98199d6cda42b4b5d2e9f0a
Lysa-Gomes <laysagomesmelo9@gmail.com>
2024-06-09 15:03:01 -0300

    Added base structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ECommerce } from "./pages/home/index";
+import { Route, Router, Routes, Switch } from "react-router-dom";
+
+import Cart from "./pages/cart";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
+import GlobalStyle from "./styled";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route exact path="/" Component={ECommerce} />
+        <Route exact path="/cart" Component={Cart} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
