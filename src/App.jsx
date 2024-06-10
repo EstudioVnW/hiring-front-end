3c552e694c2bb245adccc9749ec99df2caf5680a
Lysa-Gomes <laysagomesmelo9@gmail.com>
2024-06-10 04:40:40 -0300

    Removed footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { ECommerce } from "./pages/home";
+import ECommerce from "./pages/home";
 import { Route, Router, Routes, Switch } from "react-router-dom";
 
 import Cart from "./pages/cart";
 import Header from "./components/header/index";
-import Footer from "./components/footer/index";
 import GlobalStyle from "./styled";
 
 export default function App() {
@@ -16,7 +15,6 @@ export default function App() {
         <Route exact path="/" Component={ECommerce} />
         <Route exact path="/cart" Component={Cart} />
       </Routes>
-      <Footer />
     </>
   );
 }
