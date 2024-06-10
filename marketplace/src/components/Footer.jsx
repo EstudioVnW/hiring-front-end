5a61c4b4b7f03e41eb2fd52f20ac9482613d186f
Botelho <giulianobotelho@outlook.com.br>
2024-06-09 21:30:02 -0300

    first commit

---
+++ marketplace/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Rodape } from '../styles/footer'
+export default function Footer() {
+  return (
+    <>
+    <Rodape>
+    <p>&copy;  Desafio Técnico VnW - Giuliano Botelho </p>
+    </Rodape>
+    </>
+  )
+}
