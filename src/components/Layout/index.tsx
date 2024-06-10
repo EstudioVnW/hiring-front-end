619303186c26065110b124a165e6ff50600af51f
mateusisrael <mateuss.i.fernandes@gmail.com>
2024-06-10 03:57:22 -0300

    adjust

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,12 @@
+import { ReactNode } from 'react'
 import { Footer } from '../Footer'
 import * as S from './styles'
 
-export const Layout = ({ children }) => {
+type LayoutProps = {
+  children: ReactNode
+}
+
+export const Layout = ({ children }: LayoutProps) => {
   return (
     <S.Container>
       {children}
