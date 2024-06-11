8411c98914696c100f4268da0db88c6275ce3a24
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-06-11 11:24:38 -0300

    melhorando barra de pesquisa

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,6 +1,6 @@
 import { Search } from 'lucide-react'
-import { FormEvent, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { FormEvent, useEffect, useState } from 'react'
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 import { Input } from '@/components/ui/input'
 
 function SearchBar() {
@@ -8,10 +8,15 @@ function SearchBar() {
   const location = useLocation()
 
   const [searchTerm, setSearchTerm] = useState('')
+  const [qs] = useSearchParams()
+  const searchParamsOrder = qs.get('search')?.toLowerCase() ?? ''
+
+  useEffect(() => {
+    setSearchTerm(searchParamsOrder)
+  }, [searchParamsOrder])
 
   const handleSearch = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
     const searchParams = new URLSearchParams(location.search)
     searchParams.set('search', searchTerm)
     const isHomePage = location.pathname === '/'
@@ -23,13 +28,20 @@ function SearchBar() {
   }
 
   return (
-    <form onSubmit={handleSearch} className="flex">
+    <form
+      onSubmit={handleSearch}
+      className="flex w-full md:max-w-[350px] max-w-[150px]  "
+    >
       <Input
+        className="text-black w-full rounded-tl-md rounded-bl-md rounded-tr-none rounded-br-none focus-visible:ring-0  focus-visible:ring-offset-0 "
         placeholder="Pesquisar..."
         value={searchTerm}
         onChange={e => setSearchTerm(e.target.value)}
       />
-      <button type="submit">
+      <button
+        type="submit"
+        className="text-black bg-[#febd69] hover:bg-[#c49251]  transition-all ease-in-out p-1 rounded-br-md rounded-tr-md "
+      >
         <Search />
       </button>
     </form>
